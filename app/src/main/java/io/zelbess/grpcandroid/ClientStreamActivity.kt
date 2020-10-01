b056834526ebdcbae7ec1746b09fcd64f0f2dac2
Marta Nowaczyk <marta@zelbess.io>
2020-10-01 22:44:33 +0100

    a bit of cleanup on rx

--- app/src/main/java/io/zelbess/grpcandroid/ClientStreamActivity.kt
+++ app/src/main/java/io/zelbess/grpcandroid/ClientStreamActivity.kt
@@ -1,30 +1,18 @@
 package io.zelbess.grpcandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
-import io.grpc.stub.StreamObserver
-import io.reactivex.Observable
-import io.reactivex.ObservableEmitter
-import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import io.zelbess.grpcandroid.server.SERVER_HOST_EMULATOR
 import io.zelbess.grpcandroid.server.SERVER_PORT
-import io.zelbess.grpcprototype.GreeterGrpc
-import io.zelbess.grpcprototype.stream.HelloReply
-import io.zelbess.grpcprototype.stream.HelloRequest
-import io.zelbess.grpcprototype.stream.StreamingGreeterGrpc
 import io.zelbess.tripupdates.RxTripServiceGrpc
 import io.zelbess.tripupdates.StartTripRequest
-import io.zelbess.tripupdates.TripServiceGrpc
 import kotlinx.android.synthetic.main.activity_client_stream.*
-import kotlinx.coroutines.flow.asFlow
-import java.util.*
 
 class ClientStreamActivity : AppCompatActivity() {
 
@@ -41,19 +29,15 @@ class ClientStreamActivity : AppCompatActivity() {
         stream.setOnClickListener {
             val request = StartTripRequest.newBuilder().setId(requestCount).build()
 
-
             tripService.startTrip(request)
                 .subscribeOn(Schedulers.io())
                 .map { it.trip }
                 .observeOn(AndroidSchedulers.mainThread())
-                .doOnNext {
-                    updates.text = "Request no: ${it.id} message: ${it.message}"
-                }
-                .doOnComplete {
-                    updates.text = "Completed"
-                }
-                .doOnError { t -> showMessage("Error! ${t.message}") }
-                .subscribe()
+                .subscribe(
+                    { updates.text = "Request no: ${it.id} message: ${it.message}" },
+                    { showMessage("Error! ${it.message}") },
+                    { updates.text = "Completed" }
+                )
                 .let { disposables.add(it) }
 
             requestCount++
