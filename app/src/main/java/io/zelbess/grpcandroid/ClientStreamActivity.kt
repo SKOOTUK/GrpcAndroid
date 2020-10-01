9937c864f4cb7d460ca797ec0745a7274ec3ef26
Marta Nowaczyk <marta@zelbess.io>
2020-10-01 22:00:20 +0100

    Super simple streaming client + implementation of RxGrpc

--- app/src/main/java/io/zelbess/grpcandroid/ClientStreamActivity.kt
+++ app/src/main/java/io/zelbess/grpcandroid/ClientStreamActivity.kt
@@ -2,61 +2,77 @@ package io.zelbess.grpcandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 import io.grpc.stub.StreamObserver
+import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import io.zelbess.grpcandroid.server.SERVER_HOST_EMULATOR
+import io.zelbess.grpcandroid.server.SERVER_PORT
+import io.zelbess.grpcprototype.GreeterGrpc
 import io.zelbess.grpcprototype.stream.HelloReply
 import io.zelbess.grpcprototype.stream.HelloRequest
 import io.zelbess.grpcprototype.stream.StreamingGreeterGrpc
+import io.zelbess.tripupdates.RxTripServiceGrpc
+import io.zelbess.tripupdates.StartTripRequest
+import io.zelbess.tripupdates.TripServiceGrpc
 import kotlinx.android.synthetic.main.activity_client_stream.*
+import kotlinx.coroutines.flow.asFlow
+import java.util.*
 
 class ClientStreamActivity : AppCompatActivity() {
 
     lateinit var channel: ManagedChannel
+    private val disposables = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_stream)
 
-        val streamRequests = setupStream()
+        val tripService = setupStream()
 
+        var requestCount = 0
         stream.setOnClickListener {
-            val message = message.text.toString()
-            val request = HelloRequest.newBuilder().setName(message).build()
-            streamRequests.onNext(request)
+            val request = StartTripRequest.newBuilder().setId(requestCount).build()
 
+
+            tripService.startTrip(request)
+                .subscribeOn(Schedulers.io())
+                .map { it.trip }
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext {
+                    updates.text = "Request no: ${it.id} message: ${it.message}"
+                }
+                .doOnComplete {
+                    updates.text = "Completed"
+                }
+                .doOnError { t -> showMessage("Error! ${t.message}") }
+                .subscribe()
+                .let { disposables.add(it) }
+
+            requestCount++
         }
     }
 
-    private fun setupStream(): StreamObserver<HelloRequest> {
-        val host = host.text.toString()
-        val port = port.text.toString().toInt()
-        channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build()
+    private fun setupStream(): RxTripServiceGrpc.RxTripServiceStub {
+        channel = ManagedChannelBuilder.forAddress(SERVER_HOST_EMULATOR, SERVER_PORT).usePlaintext().build()
+        return RxTripServiceGrpc.newRxStub(channel)
 
-        val streamObserver = object : StreamObserver<HelloReply> {
-            override fun onNext(value: HelloReply?) {
-                showMessage("Reply: ${value?.message}")
-            }
-
-            override fun onError(t: Throwable?) {
-                showMessage("Error: ${t?.message.orEmpty()}")
-            }
-
-            override fun onCompleted() {
-            }
-        }
-        return StreamingGreeterGrpc.newStub(channel).sayHelloStreaming(streamObserver)
     }
 
-    fun showMessage(message: String) {
-        runOnUiThread {
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-        }
+    private fun showMessage(message: String) {
+        updates.text = message
     }
 
     override fun onDestroy() {
         channel.shutdown()
+        disposables.clear()
         super.onDestroy()
     }
 }
---
+++ app/src/main/java/io/zelbess/grpcandroid/server/ServerConfig.kt
@@ -0,0 +1,4 @@
+package io.zelbess.grpcandroid.server
+
+val SERVER_HOST_EMULATOR = "10.0.2.2"
+val SERVER_PORT = 50051
